90897ccfbe394ce10aa003b7a6fb063bb0d136df
Tim Polozkov <tim@Tims-MacBook-Air.local>
2017-06-04 23:36:21 +0300

    Allow creating and viewing projects

--- Mitarbet/urls.py
+++ Mitarbet/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout,{'next_page':'/login'}, name='logout'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^projects/', projects_views.index, name='projects'),
+    url(r'^projects/$', projects_views.index, name='projects'),
+    url(r'^projects/new/$', projects_views.add_project, name='add_project'),
+    url(r'^projects/(?P<project_id>[0-9]+)/$', projects_views.project, name='project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from .models import Project 
+from .models import Project
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     all_projects = Project.objects.order_by('-creation_date')
     context = {'all_projects':all_projects}
     return render(request, 'projects/projects.html', context)
+
+def add_project(request):
+    if request.method == 'GET':
+        return render(request,'projects/new.html', {})
+    else:
+        project_name = request.POST['project_name']
+        try:
+            if project_name == '':
+                raise Exception('Name cannot be blank')
+
+            project = Project.objects.create(project_name=project_name, creation_date=datetime.datetime.today())
+            return HttpResponseRedirect(reverse('project', args=(project.id,)))
+        except Exception as e:
+            return render(request, 'projects/new.html', {'error': str(e)})
+
+def project(request, project_id):
+    project = Project.objects.get(pk=project_id)
+    return render(request, 'projects/project.html', {'project': project})
